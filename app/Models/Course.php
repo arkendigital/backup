b8af34f9b38fee5d860f0faf04f76df0c42a910d
Stephen <stephen@Fifteens-MacBook-Pro.local>
2018-03-21 10:47:16 +0000

    course sluggable model update

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Course extends Model {
 
-  use SoftDeletes;
+  use SoftDeletes, Sluggable;
 
   /**
   * The attributes that are mass assignable.
