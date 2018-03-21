e0d67e480135d512d88f345a9164a0df7c94d5fe
Stephen <stephen@Fifteens-MacBook-Pro.local>
2018-03-21 10:28:15 +0000

    employer admin update

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Employer extends Model {
 
-  use SoftDeletes;
+  use SoftDeletes, Sluggable;
 
   /**
   * The attributes that are mass assignable.
