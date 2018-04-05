1bff5b02bd73e16ed11c5b069fde381b2439dc4b
Jack Whiting <jack@fifteen.co.uk>
2018-04-05 11:41:49 +0100

    Running php-cs-fixer

--- app/Http/Controllers/SalarySurvey/TakePartController.php
+++ app/Http/Controllers/SalarySurvey/TakePartController.php
@@ -2,15 +2,8 @@
 
 namespace App\Http\Controllers\SalarySurvey;
 
-/**
-* Load modules.
-*/
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
-/**
-* Load models.
-*/
 use App\Models\Section;
 use App\Models\Page;
 use App\Models\SalarySurvey;
@@ -18,40 +11,23 @@ use App\Models\SalarySurvey;
 class TakePartController extends Controller
 {
 
-  /**
-  * Display the page with the questionnaire.
-  *
-  */
+    /**
+     * Display the page with the questionnaire.
+     *
+     */
     public function index()
     {
-
-    /**
-    * Get page information.
-    *
-    */
         $page = Page::getPage(request()->route()->uri);
 
-        /**
-        * Set SEO.
-        *
-        */
         $this->seo()->setTitle($page->meta_title);
         $this->seo()->setDescription($page->meta_description);
 
-        /**
-        * Get adverts for this page.
-        *
-        */
         $page_adverts = getArrayOfAdverts($page->id);
 
-        /**
-        * Display page.
-        *
-        */
         return view("salary-survey.survey", compact(
-      "page",
-      "page_adverts"
-    ));
+            "page",
+            "page_adverts"
+        ));
     }
 
     /**
@@ -62,25 +38,23 @@ class TakePartController extends Controller
     */
     public function submit(Request $request)
     {
+        // @todo: No validation
         if (auth()->check()) {
             $user_id = auth()->user()->id;
         } else {
             $user_id = 0;
         }
 
-        /**
-        * Add answers to database.
-        *
-        */
+        // Add answers to database.
         $answers = SalarySurvey::create([
-      "type" => request()->type,
-      "sector" => request()->sector,
-      "field" => request()->field,
-      "experience" => request()->experience,
-      "qualifications" => request()->qualifications,
-      "annual_salary" => request()->annual_salary,
-      "user_id" => $user_id
-    ]);
+            "type" => request()->type,
+            "sector" => request()->sector,
+            "field" => request()->field,
+            "experience" => request()->experience,
+            "qualifications" => request()->qualifications,
+            "annual_salary" => request()->annual_salary,
+            "user_id" => $user_id
+        ]);
 
         return "OK";
     }
