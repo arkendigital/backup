4d19e80ad80a25ef0de40c397f6756c4c099ecd2
Jack Whiting <jack@fifteen.co.uk>
2018-04-06 10:04:37 +0100

    Adding jquery to the walth of info tabs

--- resources/views/widgets/wealth-of-information.blade.php
+++ resources/views/widgets/wealth-of-information.blade.php
@@ -9,7 +9,7 @@
     <div class="clear"></div>
 
     <div>
-      <div class="homepage-information-box homepage-information-box-thinking">
+      <div class="homepage-information-box homepage-information-box-thinking" data-box="thinking">
         <div class="homepage-information-box-icon">
           <img src="/images/icons/homepage/thinking.png" alt="I'm thinking of becoming an Actuary" title="I'm thinking of becoming an Actuary">
         </div>
@@ -17,15 +17,15 @@
         <p class="homepage-information-box-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum</p>
       </div>
 
-      <div class="homepage-information-box homepage-information-box-studying">
+      <div class="homepage-information-box homepage-information-box-studying" data-box="studying">
         <div class="homepage-information-box-icon">
-          <img src="/images/icons/homepage/studying.png" alt="I'm thinking of becoming an Actuary" title="I'm thinking of becoming an Actuary">
+          <img src="/images/icons/homepage/studying.png" alt="I'm thinking of becoming an Actuary" title="I'm thinking of becoming an Actuary" >
         </div>
         <p class="homepage-information-box-title">I'm studying to becoming an actuary</p>
         <p class="homepage-information-box-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum</p>
       </div>
 
-      <div class="homepage-information-box homepage-information-box-iam">
+      <div class="homepage-information-box homepage-information-box-iam" data-box="iam">
         <div class="homepage-information-box-icon">
           <img src="/images/icons/homepage/iam.png" alt="I'm thinking of becoming an Actuary" title="I'm thinking of becoming an Actuary">
         </div>
@@ -36,45 +36,96 @@
 
     <div class="clear"></div>
 
-    <div class="homepage-information-box-additional">
+    <div class="homepage-information-box-additional" id="thinking">
       <div class="homepage-information-box-additional-tabs">
-        <a class="bg-orange">Topic one</a>
-        <a class="bg-orange">Topic two</a>
-        <a class="bg-orange">Topic three</a>
-        <a class="bg-orange">Topic four</a>
+        <a href="#" data-content="thinking_topic_one" class="bg-orange">Topic one</a>
+        <a href="#" data-content="thinking_topic_two" class="bg-orange">Topic two</a>
+        <a href="#" data-content="thinking_topic_three" class="bg-orange">Topic three</a>
+        <a href="#" data-content="thinking_topic_four" class="bg-orange">Topic four</a>
       </div>
 
-      <div class="homepage-information-box-additional-content">
-        <p class="homepage-information-box-additional-content-title">Topic one</p>
-        <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+      <div class="homepage-information-box-additional-content thinking-content">
+        <div id="thinking_topic_one">        
+          <p class="homepage-information-box-additional-content-title">Thinking</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="thinking_topic_two" style="display: none">        
+          <p class="homepage-information-box-additional-content-title">Thinking</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="thinking_topic_three" style="display: none">        
+          <p class="homepage-information-box-additional-content-title">Thinking</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="thinking_topic_four" style="display: none">        
+          <p class="homepage-information-box-additional-content-title">Thinking</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
       </div>
     </div>
 
-    <div class="homepage-information-box-additional homepage-information-box-additional-hidden">
+    <div class="homepage-information-box-additional homepage-information-box-additional-hidden" id="studying">
       <div class="homepage-information-box-additional-tabs">
-        <a class="bg-orange">Topic one</a>
-        <a class="bg-orange">Topic two</a>
-        <a class="bg-orange">Topic three</a>
-        <a class="bg-orange">Topic four</a>
+        <a href="#" data-content="studying_topic_one" class="bg-orange">Topic one</a>
+        <a href="#" data-content="studying_topic_two" class="bg-orange">Topic two</a>
+        <a href="#" data-content="studying_topic_three" class="bg-orange">Topic three</a>
+        <a href="#" data-content="studying_topic_four" class="bg-orange">Topic four</a>
       </div>
 
-      <div class="homepage-information-box-additional-content">
-        <p class="homepage-information-box-additional-content-title">Topic one</p>
-        <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+      <div class="homepage-information-box-additional-content studying-content">
+        <div id="studying_topic_one">      
+          <p class="homepage-information-box-additional-content-title">Studying</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="studying_topic_two" style="display: none">      
+          <p class="homepage-information-box-additional-content-title">Studying</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="studying_topic_three" style="display: none">      
+          <p class="homepage-information-box-additional-content-title">Studying</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="studying_topic_four" style="display: none">      
+          <p class="homepage-information-box-additional-content-title">Studying</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
       </div>
     </div>
 
-    <div class="homepage-information-box-additional homepage-information-box-additional-hidden">
+    <div class="homepage-information-box-additional homepage-information-box-additional-hidden" id="iam">
       <div class="homepage-information-box-additional-tabs">
-        <a class="bg-orange">Topic one</a>
-        <a class="bg-orange">Topic two</a>
-        <a class="bg-orange">Topic three</a>
-        <a class="bg-orange">Topic four</a>
+        <a href="#" data-content="iam_topic_one" class="bg-orange">Topic one</a>
+        <a href="#" data-content="iam_topic_two" class="bg-orange">Topic two</a>
+        <a href="#" data-content="iam_topic_three" class="bg-orange">Topic three</a>
+        <a href="#" data-content="iam_topic_four" class="bg-orange">Topic four</a>
       </div>
 
-      <div class="homepage-information-box-additional-content">
-        <p class="homepage-information-box-additional-content-title">Topic one</p>
-        <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+      <div class="homepage-information-box-additional-content iam-content">
+        <div id="iam_topic_one">
+          <p class="homepage-information-box-additional-content-title">IAM</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="iam_topic_two" style="display: none;">
+          <p class="homepage-information-box-additional-content-title">IAM 2</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="iam_topic_three" style="display: none;">
+          <p class="homepage-information-box-additional-content-title">IAM 3</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
+
+        <div id="iam_topic_four" style="display: none;">
+          <p class="homepage-information-box-additional-content-title">IAM 4</p>
+          <p class="homepage-information-box-additional-content-text">Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut int facea am inciuscius. Ut eos enisciaspe ma dolorpo rerunti oreseque di ulparum etur seque vellaut intfacea am inciuscius.</p>
+        </div>
       </div>
     </div>
 
@@ -104,3 +155,26 @@
 
   </div><!-- /.website-container -->
 </div><!-- /.homepage-information -->
+
+
+@push('scripts-after')
+  <script>
+    $('.homepage-information-box').click(function() {
+      var box = $(this).data('box');
+      $('.homepage-information-box-additional').addClass('homepage-information-box-additional-hidden');
+      $('#' + box).removeClass('homepage-information-box-additional-hidden');
+
+      $('.homepage-information-box-additional-content > div').hide();      
+      $('.' + box + '-content > div').first().show();
+    });
+
+    $('.homepage-information-box-additional-tabs a').click(function(e) {
+      e.preventDefault();
+
+      var box = $(this).data('content');
+      $('.homepage-information-box-additional-content > div').hide();
+      $('#' + box).show();
+    });
+  </script>
+
+@endpush
