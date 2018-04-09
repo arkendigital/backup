96806821c20ef9a1e6f5741d3a35c5466a11864b
Jack Whiting <jack@fifteen.co.uk>
2018-04-09 15:13:50 +0100

    Styling footer links based on social network

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,9 +4,9 @@
 
   <div class="footer-social">
     <span>FIND US ON SOCIAL MEDIA</span>
-    <a href="{{ $facebook }}" target="_blank"><i class="fab fa-facebook"></i></a>
-    <a href="{{ $twitter }}" target="_blank"><i class="fab fa-twitter-square"></i></a>
-    <a href="{{ $linkedin }}" target="_blank"><i class="fab fa-linkedin"></i></a>
+    <a href="{{ $facebook }}" target="_blank" class="footer-social--facebook"><i class="fab fa-facebook"></i></a>
+    <a href="{{ $twitter }}" target="_blank" class="footer-social--twitter"><i class="fab fa-twitter-square"></i></a>
+    <a href="{{ $linkedin }}" target="_blank" class="footer-social--linkedin"><i class="fab fa-linkedin"></i></a>
   </div>
 
   <nav class="footer-nav">
