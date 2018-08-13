13d29669075185463d334d505a96fb8c20e06c20
Mikey Burton <mikey@fifteen.co.uk>
2018-08-13 10:24:43 +0100

    Add translations for the settings:

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -3,4 +3,8 @@
 return [
     'site_name' => 'Site Name',
     'site_version' => 'Site Version',
+    'twitter' => 'Twitter URL',
+    'facebook' => 'Facebook URL',
+    'linkedin' => 'LinkedIn URL',
+    'admin_email' => 'Administrator Email',
 ];
