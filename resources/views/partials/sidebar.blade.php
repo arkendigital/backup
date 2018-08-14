cdf3c690bcaece6f58e9d06a798956db2ffc2ccc
Mikey Burton <mikey@fifteen.co.uk>
2018-08-14 12:08:41 +0100

    Hacky way of making the external links open in a new tab

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,14 @@
-@if($sidebar)
-  <h4 class="sidebar-menu--title">Explore More</h4>
-  @foreach($sidebar->getItems() as $item)
-    <a href="{{ $item->url }}" @if($item->url == "/".request()->path()) class="active" @endif>{{ $item->text }}</a>
-  @endforeach
+@if ($sidebar)
+    <h4 class="sidebar-menu--title">Explore More</h4>
+    @foreach($sidebar->getItems() as $item)
+        <a
+                href="{{ $item->url }}"
+                @if($item->url == "/".request()->path())
+                class="active"
+                @endif
+                @if (str_contains($item->url, 'http'))
+                target="_blank"
+                @endif
+        >{{ $item->text }}</a>
+    @endforeach
 @endif
