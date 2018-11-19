70652200f000f3ae593bad30dbe9688f6deebd0d
Romin <romin@Fifteens-iMac-7.local>
2018-11-19 10:27:03 +0000

    update a wealth of info section

--- resources/views/widgets/salary-survey.blade.php
+++ resources/views/widgets/salary-survey.blade.php
@@ -1,16 +1,16 @@
 <div class="box-select box-select--dark-blue" style="background-image:url(/images/temp/homepage-wealth-information-bg.png);">
   <div class="box-select-container">
 
-    <p class="box-select-title">A wealth of information</p>
-    <p class="box-select-text">Surveys, salary expectations and more</p>
+    <p class="box-select-title">{{ $page->getField("wealth_info_title") }}</p>
+    <p class="box-select-text">{{ $page->getField("wealth_info_sub_title") }}</p>
 
-    <a class="box-select-item" href="/jobs/vacancies">Jobs</a>
-    <a class="box-select-item" href="/salary-survey/take-part">Surveys</a>
-    <a class="box-select-item" href="/salary-survey/results">Salary</a>
+    <a class="box-select-item" href="/jobs/vacancies">{{ $page->getField("wealth_info_vacancies_text") }}</a>
+    <a class="box-select-item" href="/salary-survey/take-part">{{ $page->getField("wealth_info_survays_text") }}</a>
+    <a class="box-select-item" href="/salary-survey/results">{{ $page->getField("wealth_info_salary_text") }}</a>
 
     <div class="clear"></div>
 
-    <a href="/discussion" class="box-select-button margin-top--small">Join our discussion</a>
+    <a href="/discussion" class="box-select-button margin-top--small">{{ $page->getField("wealth_info_discussion_button") }}</a>
 
   </div>
 </div><!-- /.box-select -->
