db798353889dae4c50b89821ce495ebbd47640cb
Romin <romin@Romins-iMac.local>
2019-02-13 09:38:51 +0000

    Facing an issue of image being to small -- facebook issue

--- app/Http/Controllers/Job/JobVacanciesController.php
+++ app/Http/Controllers/Job/JobVacanciesController.php
@@ -291,7 +291,7 @@ class JobVacanciesController extends Controller
         */
         $this->seo()->setTitle($job->title);
         $this->seo()->setDescription($job->excerpt);
-        $this->seo()->opengraph()->addImage($job->company->logo);
+        $this->seo()->opengraph()->addImage($job->company->logo, ['height' => 300, 'width' => 300]);
 
         /**
         * Display job.
