755ac531488b7e335d8ea1c0590c6a7011ddd699
Joe <j.girgis85@gmail.com>
2020-02-09 19:18:57 +0000

    Export jobs stats feature for admins

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -39,4 +39,57 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-3">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Jobs Stats</h3>
+                </div>
+                <div class="box-body">
+                    <form action="{{ route("export.jobs-stats") }}" style="float:left;">
+                        <div class="input-group" style="margin-bottom:20px;">
+                          <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                          </div>
+                          <input type="text" class="form-control pull-right daterange" id="reservation" name="dates" value="{{ \Carbon\Carbon::now()->startOfMonth()->format('d-m-Y') }} - {{ \Carbon\Carbon::now()->endOfMonth()->format('d-m-Y') }}">
+                        </div>
+                        <button class="btn btn-primary" type="submit">Download Selected Range</button>
+                        <a class="btn btn-info" href="{{ route("export.jobs-stats") }}">Download {{ date('F') }} Data</a>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@push("scripts-after")
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script>
+        $(function() {
+          $(".daterange").daterangepicker({
+            locale: {
+              format: 'DD-MM-Y'
+            }
+          });
+        });
+    </script>
+@endpush
+
 @stop
+
+@push("scripts-after")
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script>
+    $(function() {
+      $(".daterange").daterangepicker({
+        locale: {
+          format: 'DD-MM-Y'
+        }
+      });
+    });
+    </script>
+  @endpush
+
