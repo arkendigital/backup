1019605e2297b3b5648f72331c40c0e991f24cbb
Joe <j.girgis85@gmail.com>
2020-12-27 00:08:47 +0000

    4,5,6,7,11 and 12 from modifications email

--- routes/jobs.php
+++ routes/jobs.php
@@ -5,7 +5,11 @@ Route::namespace('Job')->group(function() {
   Route::get('/jobs/vacancies-per-page/{perPage}', 'JobVacanciesController@perPage');
   Route::get('/actuary-jobs', 'JobVacanciesController@index');
   Route::post('/actuary-jobs', 'JobVacanciesController@set_filtering');
-  Route::get('/actuary-jobs/{job}', 'JobVacanciesController@view')->name('job.show');
+
+  Route::get('/actuary-jobs/{pageorjob}', 'JobVacanciesController@index');
+  // Route::get('/actuary-jobs/{job}', 'JobVacanciesController@view')->name('job.show');
+
+
   Route::post('/actuary-jobs/{id}/track', 'JobVacanciesController@track');
   Route::get('/jobs/advertise-with-us', 'JobAdvertiseController@index');
   Route::post('/jobs/advertise-with-us', 'JobAdvertiseController@submit');
